01367d93951fad095e9205d0fff6e2650a81a53e
arpitmandal <workforarpit@gmail.com>
2019-06-27 11:06:06 +0530

    Story modified
    Signed-off-by: arpitmandal <workforarpit@gmail.com>

--- story/templatetags/custom_tags.py
+++ story/templatetags/custom_tags.py
@@ -1,9 +1,9 @@
 from django import template
-
+from django.utils import timezone
 from story.models import Story
 
 register = template.Library()
 
 @register.filter
-def story(num_post):
-    return Story.objects.order_by('-pub_date')[:num_post]
+def story(args):
+    return Story.objects.order_by('-pub_date')
--- story/views.py
+++ story/views.py
@@ -10,35 +10,43 @@ from .models import Story
 from source.models import Sources
 from django.utils import timezone
 from django.template import loader
-    #s = Story(sources=sources,title=title, url=url)
+from django.db.models.aggregates import Count
+from random import randint
 
-def view_stories(request, id):
-    num_post = 15
+def view_stories(request):
+    source_id = request.GET('source_id')
+    num_post = 20
+    args={}
         # adding stories in database
-    try: 
-        rss_url = Sources.objects.get(id=id).rss_text
-    except Sources.DoesNotExist:
-        alert("Does not Exist")
-    feed = feedparser.parse(rss_url)
-    for post in Story.objects.all():
-        post.delete()
-    story_urls = Story.objects.filter(id=id).values_list('url', flat=True)
-    for entry in feed['entries']:
-            url = entry.get('link')
-            if url not in story_urls:
-                story = Story()
-                story.title = entry.get('title')
-                story.url = url
-                date=entry.get('published')
-                story.pub_date = dateutil.parser.parse(date)
-                story.body_text = entry.get('description')
-                story.save()
+    query_params = {}
+    if source_id:
+        query_params['id'] = source_id
+    source_qs = Sources.objects.filter(**query_params).values('rss_text', 'id')
+    for source in source_qs:
+        feed = feedparser.parse(source.get('rss_text'))
+
+        story_urls = Story.objects.filter(id=id).values_list('url', flat=True).distinct()
+        for entry in feed['entries']:
+                url = entry.get('link')
+                if url not in story_urls:
+                    story = Story()
+                    story.title = entry.get('title')
+                    story.url = url
+                    date=entry.get('published')
+                    story.pub_date = dateutil.parser.parse(date)
+                    body = entry.get('description')
+                    from bs4 import BeautifulSoup
+                    soup = BeautifulSoup(body)
+                    body = soup.get_text()
+                    story.body_text = body
+                    story.source_id = source.get('id')
+                    story.save()
     # passing stories data to template context and render it
-    context ={
-    "id":id
-    }
-    return render(request, 'story/story_list.html', context)
-        #return render(request, "source/story_list.html")
+    args['story']=story
+    return render(request, 'story/story_list.html', args)
 
 def newsfeed(request):
-    return render(request,'story/newsfeed.html')
+    args = {}
+    story = Story.objects.all()
+    args['story'] = story
+    return render(request, 'story/newsfeed.html', args)
